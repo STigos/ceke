d12537cbbc76c959fceb05968d03835083757c8d
STigos <sharktigo@gmail.com>
2016-01-16 15:45:15 +0500

    Добавленна папка фото, в css прописано свойство background_image

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-2.1.4.js"></script>
+    <script src="js/bootstrap.js"></script>
+</head>
+<body class="">
+<div class="collapse" id="collapsingNav">
+
+    <h3 class="media-heading">контент</h3>
+
+</div>
+
+    <nav class="navbar navbar-light bg-inverse ">
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+            &#9776;
+        </button>
+        <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+    <a class="navbar-brand" href="index.php">Кейк</a>
+
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+
+                Главная</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Контакты</a>
+            </li>
+        </ul>
+
+    </nav>
+<div class="container-fluid""
+<div class="row">
+    <div class="col-lg-4 col-lg-offset-4">
+        <form class="form" action="mailto:localhost@admin">
+            <label for="text">Отправить письмо</label><input type="text" class="mail" name="text" size="">
+            <input class="btn  btn-secondary-outline" type="submit" name="mai" value="Отправить">
+        </form>
+    </div>
+<div class="col-lg-4"><?php
+
+echo 'пробуем локальный сервер';
+?>
+</div>
+</div>
+
+
+
+
+</body>
+</html>
