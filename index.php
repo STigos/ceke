648eaf0dc4e17c6d36f40e0818e6dddaff40db9c
STigos <sharktigo@gmail.com>
2016-01-19 14:22:38 +0500

    18.01.2016 from job

--- index.php
+++ index.php
@@ -13,53 +13,48 @@
 </head>
 <body class="">
 <div class="collapse" id="collapsingNav">
-
     <h3 >контент</h3>
-
 </div>
-
     <nav class="navbar navbar-light  ">
         <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
             &#9776;
         </button>
         <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
     <h2 class="navbar-brand" href="index.php">Кейк</h2>
-
         <ul class="nav navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="index.php">
-
-                Главная</a>
+                <a class="nav-link" href="index.php">Главная</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Контакты</a>
+                <a class="nav-link" href="action.php">Тесты PHP</a>
             </li>
         </ul>
-
     </nav>
 <div class="container bg-faded""
 <div class="row">
-
 <div class="col-lg-4  col-mg-6 col-sm-12">
-    <?php
-    $i = 0;
-    while (++$i) {
-        switch ($i) {
-            case 5:
-                echo "Итерация 5<br />\n";
-                break 1;  /* Выйти только из конструкции switch. */
-            case 10:
-                echo "Итерация 10; выходим<br />\n";
-                break 2;  /* Выходим из конструкции switch и из цикла while. */
-            default:
-                break;
-
-        }
-    }
-    
-
-    ?>
+    <form action="action.php" method="post">
+        <div class="input-group-sm">
+            <input class="input-group" type="text" name="txt">
+            <button class="btn btn-sm btn-secondary-outline" type="submit">&#9566</button>
+        </div>
+    </form>
 </div>
+    <div class="col-lg-4  col-mg-6 col-sm-12">
+    <form action="action.php" method="post">
+        <div class="input-group-sm">
+            <input class="input-group" type="text" name="txt">
+            <button class="btn btn-sm btn-secondary-outline" type="submit">&#9</button>
+        </div>
+    </form>
+    </div><div class="col-lg-4  col-mg-6 col-sm-12">
+    <form action="action.php" method="post">
+        <div class="input-group-sm">
+            <input class="input-group" type="text" name="txt">
+            <button class="btn btn-sm btn-secondary-outline" type="submit">&#9</button>
+        </div>
+    </form>
+    </div>
 </div>
 </body>
 </html>
